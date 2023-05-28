a7bc16f1fb05b87491bc607f09025ce3d5449f9e
anushkapatil18 <anushkabpatil11@gmail.com>
2023-05-28 15:22:20 +0530

    update

--- weather.py
+++ weather.py
@@ -1,56 +1,100 @@
+#API_KEY = '67616e981465124e28cfcf9cc22d6dc4'
 import requests
 import json
 import sys
-
-#login OpenWeatherMap to get API key
-#https://home.openweathermap.org/api_keys
-#API_KEY = '67616e981465124e28cfcf9cc22d6dc4'
-#make a fucntion to get weather data
-#time format function
-#main function
-#check if the user has provided a city name
-#call the get_weather function
-#python weather.py london
-#python weather.py london,uk
-#python weather.py "new york"
-#python weather.py "new york,us"
-#python weather.py "new york" us
-
+import geocoder
+from tabulate import tabulate
 
 API_KEY = '67616e981465124e28cfcf9cc22d6dc4'
 
+#code to get current location
+def get_current_location():
+    g = geocoder.ip('me')
+    return g.city
+
 def get_weather(city_name):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric'
-    response = requests.get(url)
+    # Current weather API URL
+    current_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric'
 
-    if response.status_code == 200:
-        data = response.json()
-        temperature = data['main']['temp']
-        humidity = data['main']['humidity']
-        wind_speed = data['wind']['speed']
-        description = data['weather'][0]['description']
-        sunrise = data['sys']['sunrise']
-        sunset = data['sys']['sunset']
-        visibility = data['visibility'] / 1000  # Convert visibility from meters to kilometers
+    # Forecast API URL for next week
+    forecast_url = f'http://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={API_KEY}&units=metric'
 
+    # Fetch current weather data
+    current_response = requests.get(current_url)
+
+    if current_response.status_code == 200:
+        current_data = current_response.json()
+        print("\n")
         print(f"Current weather in {city_name}:")
-        print(f"Temperature: {temperature}°C")
-        print(f"Humidity: {humidity}%")
-        print(f"Wind Speed: {wind_speed} m/s")
-        print(f"Description: {description}")
-        print(f"Sunrise: {format_time(sunrise)}")
-        print(f"Sunset: {format_time(sunset)}")
-        print(f"Visibility: {visibility} km")
-    else:
-        print(f"Error fetching weather data for {city_name}. Please try again later.")
+        print(f"Temperature: {current_data['main']['temp']}°C")
+        #code for humidity variable
+        #humidity unit is in percentage
+        print(f"Humidity: {current_data['main']['humidity']}%")
+        #code for wind variable
+        #wind unit is in km/h
+        print(f"Wind: {current_data['wind']['speed']} km/h")
+        print(f"Description: {current_data['weather'][0]['description']}")
+        #line space after everyday weather
+        print("\n")
 
-def format_time(timestamp):
-    import datetime
-    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        print(f"Error fetching current weather data for {city_name}. Please try again later.")
+
+    # Fetch forecast data
+    forecast_response = requests.get(forecast_url)
+
+    if forecast_response.status_code == 200:
+        forecast_data = forecast_response.json()
+    
+        table_data = []
+        headers = ["Date", "Time", "Temperature (°C)","Humidity","Wind", "Description"]
+        prev_date = None
+        for forecast in forecast_data['list']:
+            date = forecast['dt_txt'].split()[0]
+            time = forecast['dt_txt'].split()[1]
+            temperature = forecast['main']['temp']
+            #code for humidity variable
+            humidity = forecast['main']['humidity']
+            #code for wind variable
+            wind = forecast['wind']['speed']
+            description = forecast['weather'][0]['description']
+            if prev_date and prev_date != date:
+                table_data.append([])  # Add empty row
+
+            table_data.append([date, time, temperature,humidity,wind, description])
+            prev_date = date
+            table_data.append([date, time, temperature,humidity,wind, description])
+
+        print(f"Weather forecast for the next week in {city_name}:\n")
+        print(tabulate(table_data, headers=headers, tablefmt="grid"))
+    else:
+        print(f"Error fetching forecast data for {city_name}. Please try again later.")
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please provide a city name as a command-line argument.")
     else:
-        city_name = ' '.join(sys.argv[1:])
-        get_weather(city_name)
+        print("Weather Forecast Tool")
+    print("1. Enter a city name")
+    print("2. Use current location")
+
+    choice = input("Select an option (1 or 2): ")
+
+    if choice == '1':
+        city_name = input("Enter a city name: ")
+    elif choice == '2':
+        city_name = get_current_location()
+        print(f"Detected current location: {city_name}")
+    else:
+        print("Invalid choice. Exiting...")
+        exit()
+
+    get_weather(city_name)
+
+
+
+
+
+
+
